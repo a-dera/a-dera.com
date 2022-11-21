9759e23ef25ded416c5bde8543322c3d3158b378
Amédée DERA <dera.amedee@gmail.com>
2022-11-21 11:36:48 +0000

    refact in vue

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Who am I?</template>
+    I am a typical developer (code is most of my life) really passionate about Python  programming langage,
+    Artificial Intelligence, Data Science, Blockchain, and IoT. I am one of those who believe that
+    code can save us.  
+     You can reach me on
+
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>How to reach me</template>
+
+    Mail 1: 
+    <a href="mailto:dera.amedee@gmail.com" target="_blank" rel="noopener">dera.amedee@gmail.com</a><br />
+
+    Mail 2: 
+    <a href="mailto:dera.amedee@outlook.com" target="_blank" rel="noopener">dera.amedee@outlook.com</a><br />
+
+    Mail 3: 
+    <a href="mailto:dera.amedee@a-dera.com" target="_blank" rel="noopener">dera.amedee@a-dera.com</a><br />
+
+    Phone 
+    <a href="tel:+22656833675" target="_blank" rel="noopener">+22656833675</a> *-* 
+    <a href="https://api.whatsapp.com/send/?phone=22656833675" target="_blank" rel="noopener">Whatsapp</a><br />
+
+    Github
+    <a href="https://github.com/a-dera/" target="_blank" rel="noopener">@a-dera</a><br />
+
+
+
+  </WelcomeItem>
+
+</template>
